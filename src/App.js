629f5fbecf07d3868d2798bbb46482860a3b4639
Andrii Chaplinskyi <chaa01051982@gmail.com>
2024-10-22 21:07:42 +0800

    finish p-3 added goods

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./App.css";
+import { Footer } from "./components/Footer.js";
+import { Header } from "./components/Header.js";
+import Items from "./components/Items.js";
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'chair',
+          img: 'chair_white.jpeg',
+          desc: 'ables are settlement and world objects in Fallout 4 and its add-on Vault-Tec Workshop. The chair are located under Furniture → Chairs in the workshop.',
+          category: 'chair',
+          price: '49.99',
+        },
+        {
+          id: 2,
+          title: 'table',
+          img: 'table_dark.jpeg',
+          desc: 'ables are settlement and world objects in Fallout 4 and its add-on Vault-Tec Workshop. The tables are located under Furniture → Tables in the workshop.',
+          category: 'table',
+          price: '149.99',
+        },
+        {
+          id: 3,
+          title: 'sofa',
+          img: 'sofa.jpeg',
+          desc: 'ables are settlement and world objects in Fallout 4 and its add-on Vault-Tec Workshop. The sofa are located under Furniture → sofa in the workshop.',
+          category: 'sofa',
+          price: '549.99',
+        },
+        {
+          id: 4,
+          title: 'lamp',
+          img: 'lamp.png',
+          desc: 'ables are settlement and world objects in Fallout 4 and its add-on Vault-Tec Workshop. The lamp are located under Furniture → lamp in the workshop.',
+          category: 'lamp',
+          price: '199.99',
+        },
+        {
+          id: 5,
+          title: 'chair',
+          img: 'chair_grey.jpeg',
+          desc: 'ables are settlement and world objects in Fallout 4 and its add-on Vault-Tec Workshop. The chair are located under Furniture → Chairs in the workshop.',
+          category: 'chair',
+          price: '69.99',
+        },
+      ]
+    }
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <div>
+            <span className='logo'>House Staff</span>
+            <ul className='nav'>
+              <li>About Us</li>
+              <li>Contacts</li>
+              <li>Account</li>
+
+            </ul>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
